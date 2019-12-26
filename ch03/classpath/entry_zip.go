a6017d9eeb9fcc66e40957ab9d539e1e700f009a
daixiaodong <dxd@asman.com.cn>
2019-12-26 21:25:11 +0800

    实现 class 文件结构

---
+++ ch03/classpath/entry_zip.go
@@ -0,0 +1,57 @@
+package classpath
+
+import (
+	"archive/zip"
+	"errors"
+)
+import "io/ioutil"
+import "path/filepath"
+
+type ZipEntry struct {
+	absPath string
+}
+
+func newZipEntry(path string) *ZipEntry {
+	absPath, err := filepath.Abs(path)
+	if err != nil {
+		panic(err)
+	}
+	return &ZipEntry{absPath}
+
+}
+
+func (self *ZipEntry) readClass(ClassName string) ([]byte, Entry, error) {
+
+	r, err := zip.OpenReader(self.absPath)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	defer r.Close()
+
+	// _  下划线不知代表啥
+	for _, f := range r.File {
+
+		if f.Name == ClassName {
+			rc, err := f.Open()
+			if err != nil {
+				return nil, nil, err
+			}
+
+			defer rc.Close()
+
+			data, err := ioutil.ReadAll(rc)
+			if err != nil {
+				return nil, nil, err
+			}
+			return data, self, nil
+		}
+	}
+
+	return nil, nil, errors.New("class not found: " + ClassName)
+
+}
+
+func (self *ZipEntry) String() string {
+	return self.absPath
+}
