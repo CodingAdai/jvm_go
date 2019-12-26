684e7c858836acc0fea984365425a00d8f23d340
daixiaodong <dxd@asman.com.cn>
2019-12-26 21:24:53 +0800

    实现 class 文件结构

---
+++ ch03/classfile/class_file.go
@@ -0,0 +1,60 @@
+package classfile
+
+import "fmt"
+
+type ClassFile struct {
+	minorVersion uint16
+	majorVersion uint16
+	constantPool ConstantPool
+	accessFlags  uint16
+	thisClass    uint16
+	superClass   uint16
+	interfaces   []uint16
+	fields       []*MemberInfo
+	methods      []*MemberInfo
+	attributes   []AttributeInfo
+}
+
+func Parse(classData []byte) (cf *ClassFile, err error) {
+	defer func() {
+
+		if r := recover(); r != nil {
+			var ok bool
+			err, ok = r.(error)
+			if !ok {
+				err = fmt.Errorf("%v", r)
+			}
+		}
+
+	}()
+
+	cr := &ClassReader{classData}
+
+	cf = &ClassFile{}
+
+	cf.read(cr)
+	return
+
+}
+
+func (self *ClassFile) read(reader *ClassReader) {
+	self.readAndCheckMagic(reader)
+	self.readAndcheckVersion(reader)
+
+	self.constantPool = readConstantPool(reader)
+	self.accessFlags = reader.readUint16()
+	self.thisClass = reader.readUint16()
+	self.superClass = reader.readUint16()
+	self.interfaces = reader.readUint16s()
+	self.fields = readMembers(reader, self.constantPool)
+	self.methods = readMembers(reader, self.constantPool)
+	self.attributes = readAttributes(reader, self.constantPool)
+}
+
+func (self *ClassFile) MajorVersion() uint16 {
+	return self.majorVersion
+}
+
+func (self *ClassFile) MinorVersion() uint16 {
+	return self.minorVersion
+}
